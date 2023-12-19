522949f9dc5cc07cbb5f61b2768beef407def450
bjjinlaw <bjmgtheone444@gmail.com>
2023-12-19 17:22:39 +0545

    # time: 2 hours # comment: changes into some models in attendance

--- teacher/models.py
+++ teacher/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from utils.models import User, TimeStampAbstractModel
 
 
-
-
 class TeacherProfile(TimeStampAbstractModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="teacher_user")
+    degree = models.CharField(max_length=100, blank=True, null=True)
     
